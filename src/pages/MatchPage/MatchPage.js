df0abb63948496416e580f3a5364ed893e791590
Robbery93 <89379889+Robbery93@users.noreply.github.com>
2022-06-30 18:35:14 +0200

    Docent kan nu het huiswerk bewerken!

--- src/pages/MatchPage/MatchPage.js
+++ src/pages/MatchPage/MatchPage.js
@@ -4,13 +4,15 @@ import styles from "./MatchPage.module.css"
 import robbert from "../../assets/Robbert.jpg"
 import floortje from "../../assets/Floortje.jpg"
 import Avatar from "../../components/Avatar/Avatar";
-import HomeworkField from "../../components/HomeworkField/HomeworkField";
 import Background from "../../components/StylingElements/Background/Background";
 import {AuthContext} from "../../context/AuthContext";
 import Button from "../../components/StylingElements/Button/Button";
 import NotRegistered from "../../components/NotRegistered/NotRegistered";
 import axios from "axios";
 import {useParams} from "react-router-dom";
+import { ErrorMessage } from "@hookform/error-message";
+import {useForm} from "react-hook-form";
+import Form from "../../components/FormElements/Form/Form";
 
 const MatchPage = () => {
 
@@ -18,6 +20,8 @@ const MatchPage = () => {
     
     // const { teacherId } = useParams();
     const { studentId } = useParams();
+
+    const { register, handleSubmit, formState: {errors} } = useForm({mode: "onChange"});
 
     const axiosConfig = { headers: {
             'Content-Type' : 'application/json',
@@ -27,6 +31,12 @@ const MatchPage = () => {
     const [student, setStudent] = useState({});
     // const [teacher, setTeacher] = useState({});  Toevoegen van foto moet er nog bij!
     const [dataCollected, toggleDataCollected] = useState(false)
+    const [edit, toggleEdit] = useState(false);
+
+    const onFormSubmit = (data) => {
+        console.log(data);
+        toggleEdit(!edit);
+    }
 
     
     useEffect(() => {
@@ -66,15 +76,42 @@ const MatchPage = () => {
                             </div>
 
                             <div className={styles.homework_container}>
-                                <HomeworkField
-                                    name={student.name}
-                                    homework={student.lesson[0].homework}
-                                />
-                                {user.authority === "ROLE_TEACHER" &&
-                                <Button
-                                    id={styles.edit_btn}
-                                    color="orange"
-                                    text="Huiswerk aanpassen" />
+                                <h3>Huiswerk van {student.name}</h3>
+                                {edit ?
+                                    <form className={styles.homework_form} onSubmit={handleSubmit(onFormSubmit)}>
+                                        <textarea
+                                            className={styles.homework_input}
+                                            placeholder={student.lesson[0].homework}
+                                            id="homework"
+                                            {...register("homework", {
+                                                required: "Geef het huiswerk op",
+                                                minLength: {
+                                                    value: 30,
+                                                    message: "Vul meer tekst is. Zo is het duidelijker voor de leerling wat hij/zij moet leren voor de volgende keer"
+                                                }
+                                            })}/>
+                                        <ErrorMessage errors={errors}
+                                                      name="homework"
+                                                      render={({message}) => <p>{message}</p>} />
+                                        <Button
+                                            addStyle={styles.confirm_btn}
+                                            color="green"
+                                            type="submit"
+                                            text="Bevestigen"
+                                        />
+                                    </form>
+                                    :
+                                    <>
+                                        <Background color="white" specificBackground={styles.homework_container__txt}>
+                                            <p>{student.lesson[0].homework}</p>
+                                        </Background>
+                                        {user.authority === "ROLE_TEACHER" &&
+                                        <Button
+                                            color="orange"
+                                            text="Huiswerk aanpassen"
+                                            onClick={() => toggleEdit(!edit)}/>
+                                        }
+                                    </>
                                 }
                             </div>
                         </Background>
